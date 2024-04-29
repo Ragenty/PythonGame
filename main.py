950216584059a0f9984b14105ad7c11f545169b6
Ragenty <165224376+Ragenty@users.noreply.github.com>
2024-04-29 01:01:13 -0400

    fixed me being dumb

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import random as rand
 import time as tm
 
-#Asks usr for basic information
+#Asks user for basic information
 while True:
   try:
     user_name = input("What is your name?  ")
     if user_name.isalpha():
-      print("Hello ", user_name".")
+      print("Hello ", user_name,".")
       break
     else:
       raise TypeError
   except TypeError:
     print("Please enter letters only.")
-    return False
